23be8b0c11a003250ab86ab82533fb5436dbf3c2
Code-Ac <76821666+Code-Ac@users.noreply.github.com>
2021-06-25 19:51:17 +0200

    Fixed lint error
    Original commit by @MrHallway0
    
    Co-Authored-By: Mr. Hallway <git@mrhallway.me>

--- src/common/modals/Settings/components/Java.js
+++ src/common/modals/Settings/components/Java.js
@@ -152,8 +152,8 @@ export default function MyAccountPreferences() {
           `}
         >
           Disable this to specify a custom java path to use instead of using
-          openJDK shipped with GDLauncher if that is the case select the path
-          to your Java executable. 
+          openJDK shipped with GDLauncher if that is the case select the path to
+          to your Java executable.
         </Paragraph>
         <Switch
           color="primary"
